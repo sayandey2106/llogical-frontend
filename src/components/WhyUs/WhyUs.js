e1ad39079da38cb06227ba82462ccaa0ca80df9c
Sayan <sayandey2106@gmail.com>
2023-01-27 13:40:58 +0530

    why us responsive

--- src/components/WhyUs/WhyUs.js
+++ src/components/WhyUs/WhyUs.js
@@ -16,8 +16,9 @@ export default function WhyUs() {
         <div class="row">
             <div class="container py-5">
                 <div class="col-12 col-md-12 why-phy-box d-flex align-items-center text-center p-3 cflx">
-                    <div class="col-12 col-md-3 d-flex justify-content-center"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20356.svg"/></div>
-                    <div class="col-12 col-md-9">
+                    <div className='row'>
+                    <div class="col-12 col-md-3 col-sm-12 d-flex justify-content-center"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20356.svg"/></div>
+                    <div class="col-12 col-md-9 col-sm-12">
                         <div class="d-flex flex-column">
                             <div class="why-inner-head font-fam-bold px-4">Live / Scheduled Lectures</div>
                             <div class="why-inner-text font-fam-regular px-4">Learning is an important step for achieving dreams in a
@@ -28,21 +29,23 @@ export default function WhyUs() {
                                 motivates the students to be creative and be passionate learners.</div>
                         </div>
                     </div>
+                    </div>
                 </div>
                 <div class="col-12 col-md-12 text-center">
                     <span class="d-md-block d-none"><img class="img-fluid" alt="img18" src="https://www.pw.live/version14/assets/img/group-20364.svg"/></span>
                     <span class="d-md-none"><img class="img-fluid" alt="img18" src="https://www.pw.live/version14/assets/img/mgroup20364.svg"/></span>
                 </div>
                 <div class="col-12 col-md-12 why-phy-box-inn d-flex align-items-center p-3 cflx">
+                    <div className='row'>                    
+                    <div class="col-12 col-md-3 col-sm-12 d-flex justify-content-center d-md-none"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20360.svg" width="220" height="220"/></div>
 
-                    <div class="col-12 col-md-3 d-flex justify-content-center d-md-none"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20360.svg" width="220" height="220"/></div>
-
-                    <div class="col-12 col-md-9">
+                    <div class="col-12 col-md-9 col-sm-12">
                         <div class="d-flex flex-column">
                             <div class="why-inner-head font-fam-bold px-4">Live Doubt Solving Sessions</div>
                             <div class="why-inner-text font-fam-regular px-4">At Physics Wallah we always encourage students to ask
                                 questions. We have created an atmosphere where students don’t hesitate to ask
                                 their doubts. We firmly believe in More you ask, the more you learn.</div>
+                        </div>
                         </div>
                     </div>
                     <div class="col-12 col-md-3 d-flex justify-content-center d-md-block d-none"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20360.svg" width="220" height="220"/></div>
@@ -52,14 +55,15 @@ export default function WhyUs() {
                     <span class="d-md-none"><img class="img-fluid" alt="img18" src="https://www.pw.live/version14/assets/img/mgroup20368.svg"/></span>
                 </div>
                 <div class="col-12 col-md-12 why-phy-box-inn d-flex align-items-center p-3 cflx">
-
-                    <div class="col-12 col-md-3 d-flex justify-content-center"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20358.svg"/></div>
-                    <div class="col-12 col-md-9">
+                    <div className='row'>
+                    <div class="col-12 col-md-3 col-sm-12 d-flex justify-content-center"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20358.svg"/></div>
+                    <div class="col-12 col-md-9 col-sm-12">
                         <div class="d-flex flex-column">
                             <div class="why-inner-head font-fam-bold px-4">Structured And Targeted Study Material</div>
                             <div class="why-inner-text font-fam-regular px-4">Explore the art of concept with our structured material
                                 with intelligent question tackling and problem solving skills.</div>
                         </div>
+                    </div>
                     </div>
                 </div>
                 <div class="col-12 col-md-12 text-center">
@@ -68,8 +72,9 @@ export default function WhyUs() {
                     <span class="d-md-none"><img class="img-fluid" alt="img18" src="https://www.pw.live/version14/assets/img/mgroup20364.svg"/></span>
                 </div>
                 <div class="col-12 col-md-12 why-phy-box-last d-flex align-items-center p-3 cflx">
-                    <div class="col-12 col-md-3 d-flex justify-content-center d-md-none"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20359.svg"/></div>
-                    <div class="col-12 col-md-9">
+                    <div className='row'>
+                    <div class="col-12 col-md-3 col-sm-12 d-flex justify-content-center d-md-none"><img class="img-fluid" alt="img11" src="https://www.pw.live/version14/assets/img/group-20359.svg"/></div>
+                    <div class="col-12 col-md-9 col-sm-12">
                         <div class="d-flex flex-column">
                             <div class="why-inner-head font-fam-bold px-4">Tests On Regular Basis For Progress Tracking</div>
                             <div class="why-inner-text font-fam-regular px-4">It is a set of test papers designed to make the student
@@ -78,7 +83,8 @@ export default function WhyUs() {
                                 exam hall.The problems involve multi-dimensional thinking at a time.</div>
                         </div>
                     </div>
-                    <div class="col-12 col-md-3 d-flex justify-content-center"><img class="img-fluid d-md-block d-none" alt="img11" src="https://www.pw.live/version14/assets/img/group-20359.svg"/>
+                    <div class="col-12 col-md-3 col-sm-12 d-flex justify-content-center"><img class="img-fluid d-md-block d-none" alt="img11" src="https://www.pw.live/version14/assets/img/group-20359.svg"/>
+                    </div>
                     </div>
                 </div>
             </div>
